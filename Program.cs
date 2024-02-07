cf853472345cc76380d44aa9429df819f158c03f
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2024-02-07 11:40:04 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FechaEjercicio7
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduzca su fecha de nacimiento:");
+            string fecha1 = Console.ReadLine();
+            Console.WriteLine("Introduzca dias para sumar:");
+            int dias = Convert.ToInt32(Console.ReadLine());
+
+            DateTime fechaNacimiento2 = DateTime.Parse(fecha1);
+
+            Console.WriteLine(fechaNacimiento2.AddDays(dias));
+
+            Console.WriteLine(fechaNacimiento2.AddDays(-dias));
+        }
+
+    }
+
+}
